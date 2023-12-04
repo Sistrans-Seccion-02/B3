01c4b222eaf6a3b50f343e39ac3b52a668fa51b6
juanlozano3 <juanlozanog9@gmail.com>
2023-12-03 19:20:48 -0500

    terminadooooooooooooooo, cali perdió

--- Entrega3/demo/src/main/java/com/example/demo/modelo/Habitacion.java
+++ Entrega3/demo/src/main/java/com/example/demo/modelo/Habitacion.java
@@ -1,5 +1,60 @@
 package com.example.demo.modelo;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.DBRef;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "habitacion")
 public class Habitacion {
-    
+
+    @Id
+    private String id;
+    private int nHabitacion;
+    @DBRef
+    private TipoHabitacion tipoHabitacion;
+    private int capacidad;
+
+    // Constructor sin parámetros
+    public Habitacion() {}
+
+    // Constructor con parámetros
+    public Habitacion(String id, int nHabitacion, TipoHabitacion tipoHabitacion, int capacidad) {
+        this.id = id;
+        this.nHabitacion = nHabitacion;
+        this.tipoHabitacion = tipoHabitacion;
+        this.capacidad = capacidad;
+    }
+
+    // Getters y setters
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public int getNHabitacion() {
+        return nHabitacion;
+    }
+
+    public void setNHabitacion(int nHabitacion) {
+        this.nHabitacion = nHabitacion;
+    }
+
+    public TipoHabitacion getTipoHabitacion() {
+        return tipoHabitacion;
+    }
+
+    public void setTipoHabitacion(TipoHabitacion tipoHabitacion) {
+        this.tipoHabitacion = tipoHabitacion;
+    }
+
+    public int getCapacidad() {
+        return capacidad;
+    }
+
+    public void setCapacidad(int capacidad) {
+        this.capacidad = capacidad;
+    }
 }
